57742cc99323d02a73980337e250bbb9f3eb32fb
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2023-03-19 09:40:11 +0530

    add amqp cloud connect logic

--- publisher.js
+++ publisher.js
@@ -1,4 +1,4 @@
-const amq = require("amqplib");
+const amqp = require("amqplib");
 
 const msg = { number: process.argv[2] };
 
@@ -6,5 +6,14 @@ connect();
 
 async function connect() {
   try {
-  } catch (ex) {}
+    const connection = await amqp.connect(process.env.AMQP_SERVER);
+    const channel = await connection.createChannel();
+    await channel.assertQueue("jobs");
+    await channel.sendToQueue("jobs", Buffer.from(JSON.stringify(msg)));
+    console.log(`Job sent successfully ${msg.number}`);
+    await channel.close();
+    await connection.close();
+  } catch (ex) {
+    console.error(ex);
+  }
 }
